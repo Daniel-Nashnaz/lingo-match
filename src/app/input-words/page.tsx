96b1b090992926584dd695b6dc55d0ed13a94989
Daniel-Nashnaz <daniel.81953@gmail.com>
2024-09-22 20:09:47 +0300

    [MINOR] add valid of inputs can't put empty

--- src/app/input-words/page.tsx
+++ src/app/input-words/page.tsx
@@ -8,12 +8,15 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Plus, Trash2, Save } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 import { toast, Toaster } from "react-hot-toast";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { WordPair, WordPairType } from "@/lib/types";
 import { confirmAction } from "@/lib/utils/notificationUtils";
 import { useAppContext } from "@/context/AppContext";
-import { DURATION_ERROR, NUM_OF_PAIRS, DURATION_SUCCSS } from "@/lib/utils/constants";
-
+import {
+  DURATION_ERROR,
+  NUM_OF_PAIRS,
+  DURATION_SUCCSS,
+} from "@/lib/utils/constants";
 /*
 const INITIAL_WORD_PAIRS: WordPair[] = [
   { id: uuidv4(), wordKnown: "חתול", wordLearn: "cat" },
@@ -23,6 +26,7 @@ const INITIAL_WORD_PAIRS: WordPair[] = [
   { id: uuidv4(), wordKnown: "ארנב", wordLearn: "rabbit" },
 ];
 */
+
 export default function InputPage() {
   const router = useRouter();
   const { words, setWords } = useAppContext();
@@ -34,7 +38,7 @@ export default function InputPage() {
 
   const addWordPair = useCallback(() => {
     if (newPair.wordKnown.trim() && newPair.wordLearn.trim()) {
-      setWordPairs(prev => [...prev, { ...newPair, id: uuidv4() }]);
+      setWordPairs((prev) => [...prev, { ...newPair, id: uuidv4() }]);
       setNewPair({ wordKnown: "", wordLearn: "" });
       toast.success("New word pair added", { duration: DURATION_SUCCSS });
     } else {
@@ -42,8 +46,11 @@ export default function InputPage() {
     }
   }, [newPair]);
 
-
-  const updateWordPair = (index: number, field: WordPairType, value: string) => {
+  const updateWordPair = (
+    index: number,
+    field: WordPairType,
+    value: string
+  ) => {
     if (index) {
       const newPairs = [...wordPairs];
       newPairs[index][field] = value;
@@ -53,32 +60,29 @@ export default function InputPage() {
 
   /**
    * removeWordPair Function
-   * 
+   *
    * Asynchronously removes a word pair from the state after user confirmation.
-   * 
+   *
    * @param {string} id - The unique identifier of the word pair to be removed.
-   * 
+   *
    * Process:
-   * 1. Calls `confirmAction` to display a confirmation toast asking the user to confirm 
+   * 1. Calls `confirmAction` to display a confirmation toast asking the user to confirm
    *    the deletion of the specific word pair.
    * 2. If the user confirms, the word pair is removed from the `wordPairs` state.
    * 3. Displays a success toast notification indicating the word pair has been removed.
-   * 
+   *
    * Dependencies:
    * - `confirmAction`: Used to handle the confirmation dialog and resolve the action.
-   * 
+   *
    * Usage:
-   * This function is typically called in response to a user's click event on a delete button 
+   * This function is typically called in response to a user's click event on a delete button
    * associated with a word pair.
    */
   const removeWordPair = async (index: number) => {
-    const confirmed: boolean = await confirmAction(
-      () => {
-        const newPairs = wordPairs.filter((_, i) => i !== index);
-        setWordPairs(newPairs);
-      },
-      "Are you sure you want to delete this word pair?"
-    );
+    const confirmed: boolean = await confirmAction(() => {
+      const newPairs = wordPairs.filter((_, i) => i !== index);
+      setWordPairs(newPairs);
+    }, "Are you sure you want to delete this word pair?");
     if (confirmed)
       toast.success("Word pair removed", { duration: DURATION_ERROR });
   };
@@ -89,18 +93,30 @@ export default function InputPage() {
       "Are you sure you want to delete all word pairs?"
     );
     if (confirmed)
-      toast.success("All word pairs have been deleted", { duration: DURATION_ERROR });
+      toast.success("All word pairs have been deleted", {
+        duration: DURATION_ERROR,
+      });
   };
 
   const startGame = () => {
-    if (wordPairs.length < NUM_OF_PAIRS)
-      toast.error(`Please add at least ${NUM_OF_PAIRS} word pairs`, { duration: DURATION_ERROR });
-    else {
-      setWords(wordPairs)
+    if (wordPairs.length < NUM_OF_PAIRS) {
+      toast.error(`Please add at least ${NUM_OF_PAIRS} word pairs`, {
+        duration: DURATION_ERROR,
+      });
+      return;
+    }
+    const validWordPairs = wordPairs.filter(
+      (pair) => pair.wordKnown.trim() && pair.wordLearn.trim()
+    );
+    if (validWordPairs.length < wordPairs.length) {
+      toast.error(`Please input valid word pairs`, {
+        duration: DURATION_ERROR,
+      });
+    } else {
+      setWords(wordPairs);
       router.push("/game");
     }
   };
-
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-50 to-purple-50 py-8 px-4">
